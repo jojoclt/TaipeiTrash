296e95584f011a32bd74a9811688ef0c480d4c88
Jojo Chan <Chalanthorn_jojo@hotmail.com>
2025-11-03 23:10:43 +0800

    Bump version code to 5 and update version name to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.jojodev.taipeitrash"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.1"
+        versionCode = 5
+        versionName = "1.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
