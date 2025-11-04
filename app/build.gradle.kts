a59ef6a280550ceff8c74fa85d8e82678430ebe4
Jojo Chan <Chalanthorn_jojo@hotmail.com>
2025-11-04 21:42:43 +0800

    Bump version code and name: Update to version 1.0.4 with code 7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.jojodev.taipeitrash"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.0.3"
+        versionCode = 7
+        versionName = "1.0.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
